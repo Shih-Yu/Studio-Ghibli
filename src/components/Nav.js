cd274c810b434f9811bad2da2852dbba618881e9
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-09 10:14:30 -0500

    add links to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
     <div>
-      Nav
+      <h5>株式会社スタジオジブリ</h5>
+       <Link to="/">Home</Link>
+       <Link to="/films">Films</Link>
+       <Link to="/locations">Locations</Link>
+       <Link to="/people">People</Link>
+       <Link to="/species">Species</Link>
+       <Link to="/vehicles">Vehicles</Link>
     </div>
   )
 }
