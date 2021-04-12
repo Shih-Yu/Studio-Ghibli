ddbef1d7a90c27e6899da965d51a33eeb4bebe98
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-11 21:11:47 -0500

    add style

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome To Studio Ghibli Info</h1>
-      <h2>株式会社スタジオジブリ</h2>
-    </div>
+    <>
+      <Jumbotron className="text-center mt-5 bg-info">
+        <h1>Welcome To Studio Ghibli Info</h1>
+        <h1>株式会社スタジオジブリ</h1>
+      </Jumbotron>
+    </>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 export default function Nav() {
   return (
-    <div>
-      <h5>株式会社スタジオジブリ</h5>
-       <Link to="/">Home</Link>
-       <Link to="/films">Films</Link>
-       <Link to="/locations">Locations</Link>
-       <Link to="/people">People</Link>
-       <Link to="/species">Species</Link>
-       <Link to="/vehicles">Vehicles</Link>
-    </div>
-  )
+    <>
+      <Container fluid>
+        <Navbar className="text-center justify-content-between pr-5 fs-4">
+          <Navbar.Brand className="fs-1">株式会社スタジオジブリ</Navbar.Brand>
+          <Link className="text-info" to="/">
+            Home
+          </Link>
+          <Link className="text-info" to="/films">
+            Films
+          </Link>
+          <Link className="text-info" to="/locations">
+            Locations
+          </Link>
+          <Link className="text-info" to="/people">
+            People
+          </Link>
+          <Link className="text-info" to="/species">
+            Species
+          </Link>
+          <Link className="text-info" to="/vehicles">
+            Vehicles
+          </Link>
+        </Navbar>
+      </Container>
+    </>
+  );
 }
