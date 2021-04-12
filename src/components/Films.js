c1ea4f47a4f3dee06a8a6dd88d7b9cdb5f915c0a
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-11 19:01:59 -0500

    add styling to page

--- src/components/Films.js
+++ src/components/Films.js
@@ -1,10 +1,20 @@
+// Import dependences
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+// Create functional component and export it
 export default function Films() {
+  // Declaring and setting state
   const [films, setFilms] = useState([]);
+  // Invoking useEffect to get data from api
   useEffect(
     () =>
+      // Using axios to getch from api
       axios
         .get("https://ghibliapi.herokuapp.com/films")
         .then((response) => {
@@ -17,35 +27,41 @@ export default function Films() {
               description: film.description,
               url: film.url,
               release: film.release_date,
-              director: film.director
+              director: film.director,
             }))
           );
         })
+        // catching errors and showing it
         .catch((err) => console.error(err.message)),
     []
   );
 
+  // Rendering film component
   return (
-    <div>
+    <>
       <h1>Welcome to the film page</h1>
-      {films.map((film) => (
-        <div className="row">
-          <div className="col-md-6">
-            <div className="card" key={film.id}>
-              {/* TODO fix cors issues in React */}
-              <img src={film.url} className="card-img-top" alt="movie poster" />
-              <div className="card-body">
-                <h2 className="card-title">
+      {/* Using Container/Row/Col/Cards to style the page */}
+      <Container fluid>
+        <Row>
+          {/* Mapping through the data and get each property in setState */}
+          {films.map((film) => (
+            <Col md="4" className="mb-3">
+              <Card key={film.id}>
+                {/* TODO fix cors issues in React */}
+                <img variant="top" src={film.url} alt="movie poster" />
+                <Card.Header as="h3" className="bg-info">
                   {film.title} - {film.release}
-                </h2>
-                <h3>{film.original_title}</h3>
-                <h6>{film.director}</h6>
-                <p className="card-text text-secondary">{film.description}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+                </Card.Header>
+                <Card.Body>
+                  <Card.Text as="h3">{film.original_title}</Card.Text>
+                  <Card.Text>{film.director}</Card.Text>
+                  <Card.Text className="card-text text-secondary">{film.description}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 }
