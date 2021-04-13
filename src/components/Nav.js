e458754733627bb345571d0bea1a44764c08a881
Shih-Yu Hwang <shihyuhwang@gmail.com>
2021-04-13 09:43:29 -0500

    add search feature

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 export default function Nav() {
   return (
@@ -27,6 +30,10 @@ export default function Nav() {
           <Link className="text-info" to="/vehicles">
             Vehicles
           </Link>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-info">Search</Button>
+          </Form>
         </Navbar>
       </Container>
     </>
